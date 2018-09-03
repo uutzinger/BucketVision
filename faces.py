3eec3793b6ae70989d8e15c99ca8afe98e233843
mtkessel <the.rocketredneck@gmail.com>
2018-09-03 14:28:05 -0700

    Update with files from Pi3

--- faces.py
+++ faces.py
@@ -1,19 +1,16 @@
 import cv2
+import numpy
 
 class Faces:
     """
     An OpenCV pipeline created to find faces
     """
     
-    def __init__(self, name, eyes = False):
+    def __init__(self):
         """initializes all values to presets or None if need to be set
         """
-        self.name = name
-		
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.eyes = eyes
-        if (self.eyes == True):
-            self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')        
+        self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')        
 
     def process(self, source0):
         """
@@ -24,12 +21,10 @@ class Faces:
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-			
-            if (self.eyes == True):
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]
-                eyes = self.eye_cascade.detectMultiScale(roi_gray)
-                for (ex,ey,ew,eh) in eyes:
-                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
         return img
--- frameduration.py
+++ frameduration.py
@@ -7,23 +7,24 @@ Created on Tue Jan 24 20:46:25 2017
 
 import time
 
-class FrameRate:
+class FrameDuration:
     def __init__(self):
         # store the start time, end time, and total number of frames
         # that were examined between the start and end intervals
-        self._start = None
-        self._end = None
+        self._start = 0
+        self._end = 0
+        self._accumulation = 0
         self._numFrames = 0
-        self._rate = 0.0
+        self._duration = 0.0
 
     def start(self):
         # start the timer
-        self._numFrames = 0
         self._start = time.time()
         return self
 
     def reset(self):
-        self.start()
+        self._numFrames = 0
+        self._accumulation = 0
         
     def stop(self):
         # stop the timer
@@ -32,19 +33,21 @@ class FrameRate:
     def update(self):
         # increment the total number of frames examined during the
         # start and end intervals
+        self.stop()
+        self._accumulation += self.elapsed()
         self._numFrames += 1
 
     def elapsed(self):
         # return the total number of seconds between the start and
         # end interval
-        return (time.time() - self._start)
+        return (self._end - self._start)
 
-    def fps(self):
-        # compute the (approximate) frames per second
+    def duration(self):
+        # compute the average duration
         if (self._numFrames > 10):
-            self._rate = self._numFrames / self.elapsed()
+            self._duration = self._accumulation / self._numFrames
             self.reset()
             
-        return self._rate
+        return self._duration
 
 
